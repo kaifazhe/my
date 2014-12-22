a8e0b78134b11b9d6fb6685fe8a8830ce4e37680
jiyangmeng <147946690@qq.com>
2014-12-22 16:37:53 +0800

    init

---
+++ caches/configs/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'database' => 'myphp',
+		'username' => 'root',
+		'password' => '1234',
+		'tablepre' => 'my_',
+		'charset' => 'utf8',
+		'type' => 'mysql',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		),
+);
+
+?>
---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://localhost:8888/my/',
+    'authkey' => 'qfaU8dHp9GmbdC9cN7Pe9Govk4rozkdP',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
